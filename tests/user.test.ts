de9a36edc88bcf284d3c6e4ad629804ddfec3078
FranEscobarG <escobar.gutierrez.3@gmail.com>
2024-09-23 00:29:30 -0600

    [FIX] Configuracion test

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -4,14 +4,33 @@ import { app } from '../src/app';
 describe('Registro de usuario', () => {
   it('Debería registrar un nuevo usuario', async () => {
     const response = await request(app).post('/api/v1/usuarios').send({
-      name: 'Nancy',
+      name: 'test1',
       age: 32,
-      lastname: 'Escobar', 
-      email: 'nan@gmail.com',
+      lastname: 'Pérez', 
+      email: 'test1@gmail.com',
       password: '123456',
-      rol: 'tutor',
+      rol: 'TUTOR',
     });
     expect(response.statusCode).toBe(201);
   });
 
+  it('Debería fallar si el usuario ya está registrado', async () => {
+    await request(app).post('/api/v1/usuarios').send({
+      name: 'test2',
+      age: 32,
+      lastname: 'Pérez',
+      email: 'test2@gmail.com',
+      password: '123456',
+      rol: 'TUTOR',
+    });
+    const response = await request(app).post('/api/v1/usuarios').send({
+      name: 'test2',
+      age: 32,
+      lastname: 'Pérez',
+      email: 'test2@gmail.com',
+      password: '123456',
+      rol: 'TUTOR',
+    });
+    expect(response.statusCode).toBe(400);
+  });
 });
